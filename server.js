0c7e3ca90090a879d767f4acb6908c52b084b01d
RaulBirlem <raul.birlem@gmail.com>
2024-08-22 15:48:18 -0300

    ccriando onexão com mongodb

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 import express from 'express'
-
+import publicRoutes from './routes/public.js'
 const app = express()
+
+app.use(express.json())
+
+//mongodb+srv://mongodb:<db_password>@bancodedados.0qlzesa.mongodb.net/?retryWrites=true&w=majority&appName=BancoDeDados
+
 
 // pública ->cadastrar e login, privado ->listar
 
-
+app.use('/',publicRoutes)
 
 app.listen(3000, () => console.log("server running"))
