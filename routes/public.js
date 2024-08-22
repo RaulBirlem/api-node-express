affd5eba763deacd567d37d981f2082a029c4d93
RaulBirlem <raul.birlem@gmail.com>
2024-08-22 15:59:10 -0300

    criando hash com bcrypt

--- routes/public.js
+++ routes/public.js
@@ -1,13 +1,35 @@
 import express from "express"
+import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcrypt'
 
+
+
+const prisma = new PrismaClient()
 const router = express.Router()
 
 // cadastro
 
-router.post('/cadastro', (req,res) => {
-    const user = req.body
+router.post('/cadastro', async (req,res) => {
 
-    res.status(201).json(user)
+    try {
+        
+    const user = req.body
+    const salt = await bcrypt.genSalt(10)
+    const hashPassword = await bcrypt.hash(user.password, salt)
+
+    const userDB = await prisma.user.create({
+        data:{
+            email: user.email,
+            name: user.name,
+            password: hashPassword,
+        }
+    })
+
+    res.status(201).json(userDB)
+
+    } catch (error) {
+        res.status(500).json({message:"Erro de servidor, tente novamente."})
+    }
 })
 
 export default router
